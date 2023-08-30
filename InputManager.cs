7564c4d2a0c51198d782ea12bfe946b7aea3f24e
Vincent Brodin <64703732+VincentBrodin@users.noreply.github.com>
2023-08-30 14:17:44 +0200

    Update InputManager.cs
    This commit introduces new options for customizing the look controls. Users can now invert the X and Y axes, allowing for a more personalized gameplay experience. Additionally, the sensitivity of the look controls can be adjusted, providing even greater control over the camera movement. These changes give players more flexibility in how they interact with the game and help to improve the overall gameplay experience.

--- InputManager.cs
+++ InputManager.cs
@@ -3,39 +3,41 @@ using UnityEngine;
 [AddComponentMenu("Input/Manager")]
 public class InputManager : MonoBehaviour
 {
-   [Header("Movement")]
-   public KeyCode forward = KeyCode.W;
-   public KeyCode back = KeyCode.S;
-   public KeyCode right = KeyCode.D;
-   public KeyCode left = KeyCode.A;
+    [Header("Movement")]
+    public KeyCode forward = KeyCode.W;
+    public KeyCode back = KeyCode.S;
+    public KeyCode right = KeyCode.D;
+    public KeyCode left = KeyCode.A;
+    [Header("Mouse")] public bool invertX = false;
+    public bool invertY = true;
+    public Vector2 sensitivity = Vector2.one;
+    private static InputManager _instance;
 
-   private static InputManager _instance;
+    private void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
-   private void Awake()
-   {
-      _instance = this;
-      DontDestroyOnLoad(gameObject);
-   }
+    public static Vector2 MovementDirection()
+    {
+        float x = 0;
+        float y = 0;
+        if (Input.GetKey(_instance.forward)) y += 1;
+        if (Input.GetKey(_instance.back)) y -= 1;
 
-   public static Vector2 MovementDirection()
-   {
-      float x = 0;
-      float y = 0;
-      if (Input.GetKey(_instance.forward)) y += 1;
-      if (Input.GetKey(_instance.back)) y -= 1;
+        if (Input.GetKey(_instance.right)) x += 1;
+        if (Input.GetKey(_instance.left)) x -= 1;
 
-      if (Input.GetKey(_instance.right)) x += 1;
-      if (Input.GetKey(_instance.left)) x -= 1;
-
-      return new Vector2(x, y);
-   }
+        return new Vector2(x, y);
+    }
 
     public static Vector2 Mouse()
-   {
-      float x = Input.GetAxisRaw("Mouse X");
-      float y = Input.GetAxisRaw("Mouse Y");
-      return new Vector2(x,y);
-   }
+    {
+        float x = Input.GetAxisRaw("Mouse X") * _instance.sensitivity.x * (_instance.invertX ? -1 : 1);
+        float y = Input.GetAxisRaw("Mouse Y") * _instance.sensitivity.y * (_instance.invertY ? -1 : 1);
+        return new Vector2(x,y);
+    }
 
-   //More can be added this is just a template :)
+    //More can be added this is just a template :)
 }
