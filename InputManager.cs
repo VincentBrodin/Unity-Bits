ed20a225f79d37870683c8fd2ddb67c572811e74
Vincent Brodin <64703732+VincentBrodin@users.noreply.github.com>
2023-08-31 10:34:19 +0200

    Update InputManager.cs
    Allows players to save settings on their computer between play sessions. This is done with the PlayerPrefs API in unity: https://docs.unity3d.com/ScriptReference/PlayerPrefs.html#:~:text=PlayerPrefs%20is%20a%20class%20that,data%20to%20store%20sensitive%20data.

--- InputManager.cs
+++ InputManager.cs
@@ -8,11 +8,24 @@ public class InputManager : MonoBehaviour
     public KeyCode back = KeyCode.S;
     public KeyCode right = KeyCode.D;
     public KeyCode left = KeyCode.A;
+    [Space] 
+    public KeyCode jump = KeyCode.Space;
     [Header("Mouse")] public bool invertX = false;
     public bool invertY = true;
     public Vector2 sensitivity = Vector2.one;
+    
     private static InputManager _instance;
-
+    
+    public enum Setting
+    {
+        Forward,
+        Left,
+        Right,
+        Back,
+        Jump,
+        SensX,
+        SensY,
+    }
     private void Awake()
     {
         _instance = this;
@@ -32,6 +45,15 @@ public class InputManager : MonoBehaviour
         return new Vector2(x, y);
     }
 
+    public static int Jump()
+    {
+        int j = 0;
+        
+        if (Input.GetKey(_instance.jump)) j += 1;
+
+        return j;
+    }
+
     public static Vector2 Mouse()
     {
         float x = Input.GetAxisRaw("Mouse X") * _instance.sensitivity.x * (_instance.invertX ? -1 : 1);
@@ -40,4 +62,93 @@ public class InputManager : MonoBehaviour
     }
 
     //More can be added this is just a template :)
+
+    #region Saving Settings
+    public static void SaveSettings()
+    {
+        #region Movement
+        //Forward
+        if (PlayerPrefs.HasKey(Setting.Forward.ToString()))
+        {
+            KeyCode forward = GetKeycodeFromString(PlayerPrefs.GetString(Setting.Forward.ToString()));
+            _instance.forward = forward;
+        }
+        else
+        {
+            PlayerPrefs.SetString(Setting.Forward.ToString(), _instance.forward.ToString());
+        }
+        
+        //Left
+        if (PlayerPrefs.HasKey(Setting.Left.ToString()))
+        {
+            KeyCode left = GetKeycodeFromString(PlayerPrefs.GetString(Setting.Left.ToString()));
+            _instance.left = left;
+        }
+        else
+        {
+            PlayerPrefs.SetString(Setting.Left.ToString(), _instance.left.ToString());
+        }
+        
+        //Right
+        if (PlayerPrefs.HasKey(Setting.Right.ToString()))
+        {
+            KeyCode right = GetKeycodeFromString(PlayerPrefs.GetString(Setting.Right.ToString()));
+            _instance.right = right;
+        }
+        else
+        {
+            PlayerPrefs.SetString(Setting.Right.ToString(), _instance.right.ToString());
+        }
+        
+        //Back
+        if (PlayerPrefs.HasKey(Setting.Back.ToString()))
+        {
+            KeyCode back = GetKeycodeFromString(PlayerPrefs.GetString(Setting.Back.ToString()));
+            _instance.back = back;
+        }
+        else
+        {
+            PlayerPrefs.SetString(Setting.Back.ToString(), _instance.back.ToString());
+        }
+        
+        //Jump
+        if (PlayerPrefs.HasKey(Setting.Jump.ToString()))
+        {
+            KeyCode jump = GetKeycodeFromString(PlayerPrefs.GetString(Setting.Jump.ToString()));
+            _instance.jump = jump;
+        }
+        else
+        {
+            PlayerPrefs.SetString(Setting.Jump.ToString(), _instance.jump.ToString());
+        }
+        #endregion
+
+        #region Sensitivity & Mouse
+        //Sensitivity X
+        if (PlayerPrefs.HasKey(Setting.SensX.ToString()))
+        {
+            _instance.sensitivity.x = PlayerPrefs.GetFloat(Setting.SensX.ToString());
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(Setting.SensX.ToString(), _instance.sensitivity.x);
+        }
+        
+        //Sensitivity Y
+        if (PlayerPrefs.HasKey(Setting.SensY.ToString()))
+        {
+            _instance.sensitivity.y = PlayerPrefs.GetFloat(Setting.SensY.ToString());
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(Setting.SensY.ToString(), _instance.sensitivity.y);
+        }
+        #endregion
+    }
+
+    public static KeyCode GetKeycodeFromString(string key)
+    {
+        return KeyCode.None;
+    }
+    #endregion
 }
